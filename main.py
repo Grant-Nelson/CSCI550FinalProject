345caa073371b8acbbc81ee32dd01756de5619af
Grant Nelson <grantnelson1@gmail.com>
2018-11-12 21:07:21 -0700

    Changed printTitleNames to what I had meant to call it printTableNames

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def printTitleNames(cur):
+def printTableNames(cur):
     cur.execute('SELECT name FROM sqlite_master WHERE type = "table"')
     tableNames = cur.fetchall()
     print("Table Names:")
@@ -11,6 +11,6 @@ sqlite_file = './FPA_FOD_20170508.sqlite'
 conn = sqlite3.connect(sqlite_file)
 cur = conn.cursor()
 
-printTitleNames(cur)
+printTableNames(cur)
 
 conn.close()
